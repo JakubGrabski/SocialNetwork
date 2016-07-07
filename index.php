d4738b7acdad053c60e316ea1e063eb74fb5e3c5
JakubGrabski <jgrabski1993@gmail.com>
2016-07-07 19:44:23 +0200

    index.php
    Index for my Social Network

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+	require once 'header.php';
+
+	echo "<br><span class='main'>Witaj w $appname, ";
+
+	if ($loggedin) 	echo "$user, jesteœ zalogowany(a)."
+					echo 'zarejestruj siê albo zaloguj, aby przejœæ dalej.';
+?>
+</span>
+<br>
+<br>
+</html>
